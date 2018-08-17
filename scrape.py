13362e5d3d06f70a2e41d10b3fad738c3b2641d4
dhruvi pandya <dhruvi.pandya.7294@gmail.com>
2018-08-17 19:08:58 +0530

    download images

--- scrape.py
+++ scrape.py
@@ -1,3 +1,11 @@
-def _scrape():
-    print "hello"
-    return
+import requests
+import urllib
+import os
+
+def save_image(file_name, item_link):
+        resource = urllib.request.urlopen(item_link)
+        output = open(file_name+".jpg","wb")
+        output.write(resource.read())
+        output.close()
+
+#save_image('test', 'https://images-eu.ssl-images-amazon.com/images/I/41TwNfZ5LzL._AC_UL260_SR200,260_.jpg')
--- test.py
+++ test.py
@@ -1,19 +1,22 @@
 from splinter import Browser
-import scrape
 
 with Browser('chrome') as browser:
     # Visit URL
-    url = "https://jira.rax.io/issues/?filter=-1"
+    url = "https://www.amazon.in/s/ref=sr_pg_7?fst=as%3Aoff&rh=n%3A1350387031%2Ck%3Awatches%2Cp_89%3ATITAN%7CTitan&page=7&bbn=1350387031&keywords=watches&ie=UTF8&qid=1534497340"
     browser.visit(url)
     while browser.url != url :
         print('LoGiN PlEaSe!!!!')
-    _scrape()
-    browser.fill('q', 'splinter - python acceptance testing for web applications')
+    # browser.fill('q', 'splinter - python acceptance testing for web applications')
     # Find and click the 'search' button
-    button = browser.find_by_name('btnK')
-    print(button)
+    # documentation for apis
+    # https://splinter.readthedocs.io/en/latest/drivers/chrome.html#api-docs
+    button = browser.find_by_css('[data-asin]').first
+    brands = []
+    for key, value in button:
+        print(key,': ',value )
+        # brands.append(_.text)
     # Interact with elements
-    button.click()
+    # button.click()
     if browser.is_text_present('splinter.readthedocs.io'):
         print("Yes, the official website was found!")
     else:
